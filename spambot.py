e834a1ee178273ad9f68f29563240ea959152d23
user-cube <ruicoelho@ua.pt>
2018-12-29 20:53:05 +0000

    Message count and instructions

--- spambot.py
+++ spambot.py
@@ -2,22 +2,30 @@ import pyautogui, sys
 import keyboard
 import time
 
-print("Go to chat textbox")
-time.sleep(3)
-text = pyautogui.position()
-print("Chat position acquired: {}".format(text))
+def textboxCoordinates():
+    print("Go to chat textbox")
+    time.sleep(3)
+    text = pyautogui.position()
+    print("Chat position acquired: {}".format(text))
+    return text
 
-print("Go to send button")
-time.sleep(3)
-button = pyautogui.position()
-print("Send button position acquired: {}".format(button))
+def buttonCooordinates():
+    print("Go to send button")
+    time.sleep(3)
+    button = pyautogui.position()
+    print("Send button position acquired: {}".format(button))
+    return button
 
-print("Starting...")
-n = 1
+def run(text, button):
+    n = 1
+    while not keyboard.is_pressed('esc'):
+        pyautogui.click(text)
+        pyautogui.typewrite("spam", interval=0.1)
+        pyautogui.click(button)
+        print("Message number:{}".format(n))
+        n += 1
 
-while not keyboard.is_pressed('esc'):
-    pyautogui.click(text)
-    pyautogui.typewrite("spam", interval=0.1)
-    pyautogui.click(button)
-    print("Message number: {}".format(n))
-    n += 1
+text = textboxCoordinates()
+button = buttonCooordinates()
+print("Running spambot")
+run(text, button)
